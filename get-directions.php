1c7f76c01dfcffeb2d3c847397aa3f0247514f81
llocally <llocally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-21 15:40:32 +0000

    set right v number
    git-svn-id: https://plugins.svn.wordpress.org/get-directions/tags/1.22@760075 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- get-directions.php
+++ get-directions.php
@@ -3,7 +3,7 @@
 Plugin Name: getDirections
 Plugin URI: http://llocally.com/wordpress-plugins/get-directions
 Description: Get Directions knows when your site is being viewed on a mobile or desktop. When it is on a desktop if displays a map and directions, when it is viewed on a mobile it passes the co-ordinates through to the mobiles google maps so the mobile can be used for bnavifgation. the direction map can be utilised through a shortcode or widget.
-Version: 1.21
+Version: 1.22
 Author: llocally
 Author URI: http://llocally.com/wordpress-plugins/
 License: GPLv2 or later
